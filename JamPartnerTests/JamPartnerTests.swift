2adebbccec64a837114b3e803f74975bba604940
Orian Patterson <orianpatterson@hotmail.com>
2026-03-23 11:56:55 +0100

    Initial Commit

---
+++ JamPartnerTests/JamPartnerTests.swift
@@ -0,0 +1,17 @@
+//
+//  JamPartnerTests.swift
+//  JamPartnerTests
+//
+//  Created by Orian Patterson Rivero on 23/03/2026.
+//
+
+import Testing
+@testable import JamPartner
+
+struct JamPartnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
